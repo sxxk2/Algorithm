04090950c31b99df949fee9241beb070dce3e90f
Jeongseok Lee <83942213+sxxk2@users.noreply.github.com>
2022-11-14 23:44:34 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     answer = 0
-    numbers.sort(reverse=True)
+    numbers.sort()
     
     if numbers[0] * numbers[1] > numbers[-1] * numbers[-2]:
         return numbers[0] * numbers[1]
